ae2acac2033cf7606efecbd9fd2bf458d442c676
123AayanAtif <760754@pdsb.net>
2025-02-07 13:01:33 -0500

    Create Unit1Lesson1.py

---
+++ Lessons/Unit1Lesson1.py
@@ -0,0 +1,6 @@
+# Predict: What I think will happen is that the code will show up with the name provided in the "name = 'insert name'" box. It will then follow the formatting of "'Hello', name"
+# this is a code comment
+name = "Leslie"
+print("Hello", name)
+
+# Run: What I predicted would happen was about true 
