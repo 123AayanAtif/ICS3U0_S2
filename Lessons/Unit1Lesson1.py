43438a7b2a7e6a22451987ba7e3bcd5379e4f100
123AayanAtif <760754@pdsb.net>
2025-02-07 14:13:43 -0500

    Unit1Lesson1.py

--- Lessons/Unit1Lesson1.py
+++ Lessons/Unit1Lesson1.py
@@ -1,6 +1,18 @@
-# Predict: What I think will happen is that the code will show up with the name provided in the "name = 'insert name'" box. It will then follow the formatting of "'Hello', name"
 # this is a code comment
 name = "Leslie"
 print("Hello", name)
 
+# Predict: What I think will happen is that the code will show up with the name provided in the "name = 'insert name'" box. It will then output "'Hello', Leslie"
+
 # Run: What I predicted would happen was about true 
+
+# Inspect: If I wanted to add a comma to this code, I would need to add a comma right beside the "Hello" in the text. For example "("Hello," name)
+
+# Modify:
+# this is a code comment
+name = "Aayan Atif."
+print("Hello,", name)
+
+# This modification in the code will print "Hello, Aayan Atif."
+
+# Roles of the variables in python: The variable name is a string that atores the inputted name, which is then utilized to print something like "Hello Name".
