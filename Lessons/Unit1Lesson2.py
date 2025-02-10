3ffa381219b2a3547c512771b5c5b60caad26ede
123AayanAtif <760754@pdsb.net>
2025-02-10 12:35:57 -0500

    Update Unit1Lesson2.py

--- Lessons/Unit1Lesson2.py
+++ Lessons/Unit1Lesson2.py
@@ -6,7 +6,8 @@
 amount = 24
 print("Two dozen is: ", amount)
 
-# Using the variable amount can help with readability of the code and printing "Two dozen is: " helps give more feedback and information when running it
+# Using the variable amount can help with readability of the code and printing "Two dozen is: " helps with providing feedback and giving more information when running the code
+
 # Modify 2:
 amount = 7 * 4
 print("forty-two is: ", amount)
@@ -16,15 +17,18 @@ print("twenty-eight is: ", amount)
 # or
 amount = 7 * 6
 print("forty-two is: ", amount)
+
 # Modify 3:
 amount = 8
 amount += 1
 print("The amount is: ", amount)
 # amount = amount + 1 is taking the value of the amount variable and adding one to it so 8 + 1 = 9
 # amount += 1 works the same way and adds 1 to the value of amount
+
 # Modify 4:
 amount = 8
 amount *= 2
 print(amount)
+
 # amount = amount * 2 is taking the value of amount and multiplying it by two, in this case doubling the value
 # amount *= 2 works the same way and multiplies the amount value by 2
