51cf4250bfc04a9b931e86753cf164fefef477f6
123AayanAtif <760754@pdsb.net>
2025-02-11 12:40:25 -0500

    Create Unit1Lesson3.py

---
+++ Lessons/Unit1Lesson3.py
@@ -0,0 +1,21 @@
+# Task A: My prediction is that it will output:
+Welcome to Sanjay's Koala Party!
+It's possible Sanjay can distribute  12
+cookies for each bowl.
+
+# When running the program, I found that my prediction was correct.
+
+# Fix the program so that there is only one space before and after the "12":
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute", 12)
+print("cookies for each bowl.")
+
+# Task B:
+print("Sanjay has", 6, "bowls for", 144, "cookies")
+# You can also do,
+print("Sanjay has %d bowls for %d cookies" % (6, 144))
+
+# Task C:
+print("The number of cookies per bowl is:", 144/6)
+
+# One text value and one numeric value (result of dividing two numbers using /)
