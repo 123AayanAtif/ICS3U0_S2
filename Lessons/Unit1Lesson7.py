069553f839f04d6ea424c9deb9a88f3812657972
123AayanAtif <760754@pdsb.net>
2025-02-11 13:08:10 -0500

    Create Unit1Lesson7.py

---
+++ Lessons/Unit1Lesson7.py
@@ -0,0 +1,6 @@
+# Predict: I predict that the code will output "Please input a whole number:" then it will wait for a user input. 
+# Once the user inputs a number I predict that it will output "Please input another nonzero whole number" and wait for the user input.
+# My last prediction for this code will be that it will think if the second input is a factor of the first input and display if it is a factor, but it wont display if it isnt a factor because the code isnt capable to tell the user if it isnt a factor.
+
+# Run: My prediction was correct, when I ran a number like 10 and 1, it was able to tell me that 1 is a factor of 10, but when I ran 3 and 10, it kept thinking and not showing any output. 
+# Inspect: Yes, the output matched what I did on my calculator.
