a14d035662b0a609738a77f087033d5c6dc6e603
123AayanAtif <760754@pdsb.net>
2025-02-18 12:07:16 -0500

    Update Unit1Lesson7.py

--- Lessons/Unit1Lesson7.py
+++ Lessons/Unit1Lesson7.py
@@ -4,3 +4,53 @@
 
 # Run: My prediction was correct, when I ran a number like 10 and 1, it was able to tell me that 1 is a factor of 10, but when I ran 3 and 10, it kept thinking and not showing any output. 
 # Inspect: Yes, the output matched what I did on my calculator.
+
+# Modify
+
+import math
+
+x = input("Please input a whole number: ")
+x = int(x)
+y = input("Please input another nonzero whole number")
+y = int(y)
+print("Now deciding if", y, "is a factor of", x, "...")
+rem = 0
+if (y != 0):
+  rem = x % y
+  if rem == 0:
+    print("Yes!", y, "is a factor of", x)
+
+# Modify 2
+import math
+
+x = input("Please input a whole number: ")
+x = int(x)
+y = input("Please input another nonzero whole number")
+y = int(y)
+if(y != 0):
+  rem = x % y
+  if rem == 0:
+    print("Yes!", y, "is a factor of", x)
+else:
+    print("You cannot input 0 as it can not be divided")
+
+# Modify 3
+import math
+
+x = input("Input a whole number between 1 and 20: ")
+x = int(x) 
+if ((x < 1 or x > 20)):
+    print("The number must be between 1 and 20. Try again.")
+else:
+    y = input(" Input another non-zero whole number between 1 and 20:")
+    y = int(y) 
+    if ((y < 1 or y > 20)): 
+        print("The number must be between 1 and 20.")
+    else:
+      print("Now deciding if", y, "is a factor of", x, "...")
+      if y != 0:  
+            rem = x % y
+            if rem == 0:
+                print("Yes!", y, "is a factor of", x)
+            else:
+                print("No,", y, "is not a factor of", x, "...")
